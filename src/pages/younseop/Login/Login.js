af251aa618f7693ded8dce753884c5f8ca7daa57
devseop <cocc263@gmail.com>
2022-05-12 10:47:02 +0900

    [33기 이윤섭] ADD: Login, Main 파일 추가

--- src/pages/younseop/Login/Login.js
+++ src/pages/younseop/Login/Login.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import './Login.scss';
 
-const Login = () => {
-  return <div>Login</div>;
-};
+function Login() {
+  return (
+    <div className="login">
+      <div className="wrapper">
+        <h1 className="westa-logo">Westagram</h1>
+        <div className="login-box">
+          <input
+            type="text"
+            id="id"
+            placeholder="전화번호, 사용자 이름 또는 메일"
+          />
+          <input type="password" id="password" placeholder="비밀번호" />
+          <a href="/main">
+            <button id="login-btn" disabled>
+              로그인
+            </button>
+          </a>
+        </div>
+        <a href="/" className="forgot-pw">
+          비밀번호를 잊으셨나요?
+        </a>
+      </div>
+    </div>
+  );
+}
 
 export default Login;
--- src/pages/younseop/Main/Main.js
+++ src/pages/younseop/Main/Main.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import './Main.scss';
+import Nav from '../../../Components/Nav';
+//import Post from '../../components/Post/Post';
+//import Aside from '../../components/Aside/Aside';
 
-const Main = () => {
-  return <div>Main</div>;
-};
+function Main() {
+  return (
+    <>
+      <Nav />
+      <div className="main">
+        <section>
+          <div>메인페이지입니다.</div>
+        </section>
+      </div>
+    </>
+  );
+}
 
 export default Main;
